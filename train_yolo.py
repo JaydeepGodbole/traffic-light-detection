71370839ba00471de3c8183612d64937408db408
Jaydeep <jgodbole7@gmail.com>
2019-10-08 17:11:58 +0100

    initials

---
+++ train_yolo.py
@@ -0,0 +1,25 @@
+import os
+import sys
+import yaml
+import time
+from datetime import datetime
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.models as models
+import torch.optim as optim
+from torch.nn.parameter import Parameter
+from torch.utils import data
+from tensorboardX import SummaryWriter
+
+def train(cfg, writer, logger):
+
+
+if __name__=="__main__":
+	#cfg = os.path.join("configs/conf.yaml")
+	#logdir = os.path.join('runs', os.path.basename(args.config)[:-4] , str(run_id))
+	#writer = SummaryWriter(log_dir=logdir)
+	print(datetime.now())
+	print(os.path)
